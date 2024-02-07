2d0e90ae23e7144d2e3bd7fb164b2e39ee57c40f
Eipi <3378732929@qq.com>
2024-02-07 22:07:17 +0800

    Commit on Feb. 7, 2024

---
+++ src/class38/Algo_038_1_Subsequences.java
@@ -0,0 +1,63 @@
+package class38;
+
+import java.util.HashSet;
+
+/**
+ * ClassName: Algo_038_1_Subsequences
+ * Package: class38
+ * CreateTime: 2024/2/6 20:59
+ * Description:
+ *
+ * @author tongtong zhu
+ * @version 1.0
+ */
+// https://www.nowcoder.com/practice/92e6247998294f2c933906fdedbc6e6a
+public class Algo_038_1_Subsequences {
+
+    // 生成所有子序列，但该题目要求去重
+    public static String[] generatePermutation (String str) {
+        char[] s = str.toCharArray();
+        HashSet<String> set = new HashSet<>();
+        f1(s, 0, new StringBuilder(), set);
+        String[] ans = new String[set.size()];
+        int i = 0;
+        for (String cur: set) {
+            ans[i++] = cur;
+        }
+        return ans;
+    }
+
+    // 当前s[i...] 之前已经决定的路径path
+    public static void f1(char[] s, int i, StringBuilder path, HashSet<String> set) {
+        if (i == s.length) {
+            set.add(path.toString());
+            return;
+        }
+        path.append(s[i]);   // 先加进去
+        f1(s, i + 1, path, set);
+        path.deleteCharAt(path.length() - 1);   // 然后删掉
+        f1(s, i + 1, path, set);
+    }
+
+    public static String[] generatePermutation2 (String str) {
+        char[] s = str.toCharArray();
+        HashSet<String> set = new HashSet<>();
+        f2(s, 0, new char[s.length], 0, set);
+        String[] ans = new String[set.size()];
+        int i = 0;
+        for (String cur: set) {
+            ans[i++] = cur;
+        }
+        return ans;
+    }
+
+    public static void f2(char[] s, int i, char[] path, int size, HashSet<String> set) {
+        if (i == s.length) {
+            set.add(String.valueOf(path, 0, size));
+            return;
+        }
+        path[size] = s[i];
+        f2(s, i + 1, path, size + 1, set);
+        f2(s, i + 1, path, size, set);
+    }
+}
---
+++ src/class38/Algo_038_2_Combinations.java
@@ -0,0 +1,65 @@
+package class38;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * ClassName: Algo_038_2_Combinations
+ * Package: class38
+ * CreateTime: 2024/2/7 21:23
+ * Description:
+ *
+ * @author tongtong zhu
+ * @version 1.0
+ */
+// https://leetcode.com/problems/subsets-ii/description/
+public class Algo_038_2_Combinations {
+
+    public static List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(nums);
+        f(nums, 0, new int[nums.length], 0, ans);
+        return ans;
+    }
+
+    public static void f(int[] nums, int i, int[] path, int size, List<List<Integer>> ans) {
+        if (i == nums.length) {
+            ArrayList<Integer> cur = new ArrayList<>();
+            for (int j = 0; j < size; j++) {
+                cur.add(path[j]);
+            }
+            ans.add(cur);
+            return;
+        }
+        // 数下一个不同数字的位置
+        int j = i + 1;
+        while (j < nums.length && nums[i] == nums[j]) {
+            j++;
+        }
+        f(nums, j, path, size, ans);
+        while (i < j) {
+            path[size++] = nums[i++];
+            f(nums, j, path, size, ans);
+        }
+    }
+
+    public static List<List<Integer>> subsetsWithDup2(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(nums);
+        helper(nums, 0, new ArrayList<>(), ans);
+        return ans;
+    }
+
+    public static void helper(int[] nums, int idx, List<Integer> path, List<List<Integer>> ans) {
+        ans.add(new ArrayList<>(path));
+        for (int i = idx; i < nums.length; i++) {
+            if (i > idx && nums[i] == nums[i - 1]) {
+                continue; // 去重
+            }
+            path.add(nums[i]);
+            helper(nums, i + 1, path, ans);
+            path.remove(path.size() - 1);
+        }
+    }
+}
